edb9a0ce66ba33fb57d3989f8611cb6733fcdf37
Klaus v. Gleissenthall <gleissen@ucsd.edu>
2017-09-06 18:29:15 -0700

    first version

--- src/verilog.pl
+++ src/verilog.pl
@@ -4,6 +4,8 @@
 			      mk_and/2,
 			      mk_and/3
 			     ]).
+
+:- dynamic assigned/1.
 /*
 Creates Horn clause verification conditions from a intermediate language verilog file.
 */
@@ -18,6 +20,7 @@ mk_assignments(Res) :-
 	    foreach(Rel, Res)
 	do  mk_var_name(X, XV),
 	    mk_var_name(Y, YV),
+	    assert(assigned(Y)),
 	    format_atom("ite(~p=1, ~p1=1, ~p1=0)",[XV,YV,YV], Rel)
 	).
 
@@ -28,8 +31,18 @@ mk_links(Res) :-
 	do  mk_var_name(X, XV),
 	    mk_var_name(Y, YV),
 	    mk_var_name(Z, ZV),
+	    assert(assigned(Z)),
 	    format_atom("ite((~p=1; ~p=1), ~p1=1, ~p1=0)",[XV,YV,ZV,ZV], Rel)
 	).
+
+mk_unassigned(Res) :-
+	findall(X, (register(X),\+assigned(X)), Vs),
+	(   foreach(V, Vs),
+	    foreach(Def, Defs)
+	do  mk_var_name(V, VN),
+	    format_atom('~p1=0',[VN], Def)
+	),
+	mk_and(Defs, Res).
 
 mk_next_def(Res) :-
 	findall(X, register(X), [V0|Vs]),
@@ -45,13 +58,18 @@ mk_next_def(Res) :-
 	),
 	format_atom('~p, Done, T, ~p, Done1, T1)',[S,Sp], Res).
 
-mk_init(Res) :-
+mk_sink_cond(Res) :-
+	sink(X)->
+	mk_var_name(X, XV),
+	format_atom('ite(~p1=1,Done1=1,Done1=Done)',[XV], Res).
+
+mk_reset(Res) :-
 	findall(X, register(X), Vs),
 	(   foreach(V, Vs),
 	    fromto('', In, Out, Defs)
 	do  mk_var_name(V, VN),
 	    (   source(V)->
-		format_atom('~p ~p1=1, ',[In,VN], Out)
+		format_atom('~p (~p1=1;~p1=0), ',[In,VN,VN], Out)
 	    ;   format_atom('~p ~p1=0, ',[In,VN], Out)
 	    )
 	),
@@ -64,11 +82,67 @@ mk_next(Res) :-
 	mk_and(Links, Links1),
 	mk_and(Asn1, Links1, Bd),
 	mk_next_def(Hd),
-	mk_init(Init),
+	mk_reset(Reset),
+	mk_sink_cond(Sink),
+	mk_unassigned(Un),
 	format_atom('Done=1, T1=T, Done1=Done',[], Spin),
-	format_atom('~p:=~n(~n Done=0, ~p~n; Done=0, T1=T+1, ~p~n; ~p~n).',[Hd,Init,Bd,Spin], Res).
+	format_atom('~p:=~n(~n Done=0, ~p~n; Done=0, T1=T+1, ~p, ~p,~p~n; ~p~n).',[Hd,Reset,Sink,Un,Bd,Spin], Res).
 
+mk_invariant(Res) :-
+	mk_invariant(Res, '').
+
+mk_primed_invariant(Res) :-
+	mk_invariant(Res, '1').
+
+mk_invariant(Res, Suffix) :-
+	findall(X, register(X), Vs),
+	(   foreach(V, Vs),
+	    foreach(V1, Vs1),
+	    param(Suffix)
+	do  mk_var_name(V, V0),
+	    format_atom('~p~p', [V0,Suffix], V1)
+	),
+	mk_and(Vs1, Vs2),  
+	format_atom('inv(~p, Done~p, T~p)', [Vs2, Suffix, Suffix], Res).
+
+mk_init(Res) :-
+	findall(X, register(X), Vs),
+	(   foreach(V, Vs),
+	    foreach(Def, Defs)
+	do  mk_var_name(V, V0),
+	    format_atom('~p=0', [V0], Def)
+	),
+	mk_and(Defs, Defs1),
+	format_atom('~p, Done=0, T=0.',[Defs1], Res).
+
+mk_property(Res) :-
+	mk_invariant(InvL, 'L'),
+	mk_invariant(InvR, 'R'),
+	format_atom('TL=TR :- ~p,~p, DoneL=1, DoneR=1.', [InvL, InvR], Res).
+
+mk_query_naming(Res) :-
+	findall(X, register(X), Vs),
+	mk_and(Vs, Vs1),
+	format_atom('query_naming(inv(~p, done, t)).', [Vs1], Res).
+mk_vcs(Res) :-
+	mk_init(Init),
+	mk_invariant(Inv),
+	mk_primed_invariant(Inv1),
+	mk_next_def(Next),
+	mk_property(Prop),
+	format_atom('~p:-~p~n~p :- ~p,~n~p.~n~p~n',[Inv,Init,Inv1,Next,Inv,Prop], Res).
+
+cleanup :-
+	retractall(assigned).
+
+mk_output_file(Res) :-
+	mk_query_naming(Naming),
+	mk_next(Next),
+	mk_vcs(Vcs),
+	format_atom('~p~n~p~n~p~n',[Naming,Next,Vcs], Res).
+	
 test :-
 	consult(['../examples/minimal']),
-	mk_next(Res),
+	cleanup,
+	mk_output_file(Res),
 	format('~p',[Res]).
