3e41a0a3d2ebdb8ff026d3785f20ae62f60f8b0f
Klaus v. Gleissenthall <gleissen@ucsd.edu>
2018-01-10 15:17:55 -0800

    bla

--- src/verilog.pl
+++ src/verilog.pl
@@ -14,6 +14,8 @@ mk_var_name(ID, VarName) :-
 /* Transform an atom "ID" into variable name "V_ID". */	
 	format_atom('V_~p',[ID], VarName).
 
+%-- Transition relation.
+
 mk_assignments(Res) :-
 	findall(X-Y, asn(X,Y), Ls),
 	(   foreach(X-Y, Ls),
@@ -21,7 +23,7 @@ mk_assignments(Res) :-
 	do  mk_var_name(X, XV),
 	    mk_var_name(Y, YV),
 	    assert(assigned(Y)),
-	    format_atom("ite(~p=1, ~p1=1, ~p1=0)",[XV,YV,YV], Rel)
+	    format_atom("~p=~p1",[XV,YV], Rel)
 	).
 
 mk_links(Res) :-
@@ -44,19 +46,13 @@ mk_unassigned(Res) :-
 	),
 	mk_and(Defs, Res).
 
-mk_next_def(Res) :-
-	findall(X, register(X), [V0|Vs]),
-	mk_var_name(V0, V0N),
-	format_atom('next(~p',[V0N],S0),
-	format_atom('~p1', [V0N], S0p),
-	(   foreach(V, Vs),
-	    fromto(S0, In, Out, S),
-	    fromto(S0p, Inp, Outp, Sp)
-	do  mk_var_name(V, VN),
-	    format_atom("~p, ~p",[In,VN], Out),
-	    format_atom('~p, ~p1', [Inp,VN], Outp)
-	),
-	format_atom('~p, Done, T, ~p, Done1, T1)',[S,Sp], Res).
+mk_next_def(Suffix, Res) :-
+	mk_invariant_vars(Suffix, Vs),
+	mk_and(Vs, Vs1),
+	format_atom('~p1',[Suffix], PrimedSuffix),
+	mk_invariant_vars(PrimedSuffix, VsPrimed),
+	mk_and(VsPrimed, VsPrimed1),
+	format_atom('next(~p, Done~p, ~p, Done~p1)',[Vs1,Suffix,VsPrimed1,Suffix], Res).
 
 mk_sink_cond(Res) :-
 	sink(X)->
@@ -73,64 +69,89 @@ mk_reset(Res) :-
 	    ;   format_atom('~p ~p1=0, ',[In,VN], Out)
 	    )
 	),
-	format_atom('~p Done1=0, T1=0', [Defs], Res).
+	format_atom('~p Done1=0', [Defs], Res).
 
 mk_next(Res) :-
 	mk_assignments(Asn),
-	mk_links(Links),
+%	mk_links(Links),
+	Links=[],
 	mk_and(Asn, Asn1),
 	mk_and(Links, Links1),
 	mk_and(Asn1, Links1, Bd),
-	mk_next_def(Hd),
+	mk_next_def('',Hd),
 	mk_reset(Reset),
 	mk_sink_cond(Sink),
 	mk_unassigned(Un),
 	format_atom('Done=1, T1=T, Done1=Done',[], Spin),
-	format_atom('~p:=~n(~n Done=0, ~p~n; Done=0, T1=T+1, ~p, ~p,~p~n; ~p~n).',[Hd,Reset,Sink,Un,Bd,Spin], Res).
+	format_atom('~p:=~n(~n Done=0, ~p~n; Done=0, ~p, ~p,~p~n; ~p~n).',[Hd,Reset,Sink,Un,Bd,Spin], Res).
 
-mk_invariant(Res) :-
-	mk_invariant(Res, '').
+%-- Invariants.
 
-mk_primed_invariant(Res) :-
-	mk_invariant(Res, '1').
-
-mk_invariant(Res, Suffix) :-
+mk_invariant_vars(Suffix, Vs1) :-
 	findall(X, register(X), Vs),
 	(   foreach(V, Vs),
 	    foreach(V1, Vs1),
 	    param(Suffix)
 	do  mk_var_name(V, V0),
 	    format_atom('~p~p', [V0,Suffix], V1)
-	),
+	).
+
+mk_invariant(Suffix, Res) :-
+	mk_invariant_vars(Suffix, Vs1),
 	mk_and(Vs1, Vs2),  
-	format_atom('inv(~p, Done~p, T~p)', [Vs2, Suffix, Suffix], Res).
+	format_atom('inv(~p, Done~p)', [Vs2, Suffix], Res).
+
+mk_invariant(Res) :-
+	mk_invariant('', Res).
+
+mk_primed_invariant(Res) :-
+	mk_invariant('1', Res).
+
+
+mk_relational_invariant(Res) :-
+	mk_relational_invariant('',Res).
+
+mk_primed_relational_invariant(Res) :-
+	mk_relational_invariant('1', Res).
+
+mk_relational_invariant(Suffix, Res) :-
+	format_atom('L~p', [Suffix], SuffixL),
+	format_atom('R~p', [Suffix], SuffixR),
+	mk_invariant_vars(SuffixL, VLs),
+	mk_invariant_vars(SuffixR, VRs),
+	format_atom('DoneL~p', [Suffix], DoneL),
+	format_atom('DoneR~p', [Suffix], DoneR),
+	append([DoneR|VRs], [DoneL|VLs], V1s),
+	mk_and(V1s, Vs2),  
+	format_atom('inv(~p)', [Vs2], Res).
+
+
+%-- Initial states and property.
 
 mk_init(Res) :-
-	findall(X, register(X), Vs),
-	(   foreach(V, Vs),
-	    foreach(Def, Defs)
-	do  mk_var_name(V, V0),
-	    format_atom('~p=0', [V0], Def)
-	),
-	mk_and(Defs, Defs1),
-	format_atom('~p, Done=0, T=0.',[Defs1], Res).
+	mk_invariant_vars('L=0', DefsL),
+	mk_invariant_vars('R=0', DefsR),
+	mk_and(DefsL, DefsL1),
+	mk_and(DefsR, DefsR1),
+	format_atom('~p,DoneL = 0, ~p, DoneR=0.',[DefsL1,DefsR1], Res).
 
 mk_property(Res) :-
-	mk_invariant(InvL, 'L'),
-	mk_invariant(InvR, 'R'),
-	format_atom('TL=TR :- ~p,~p, DoneL=1, DoneR=1.', [InvL, InvR], Res).
+	mk_relational_invariant(Inv),
+	format_atom('DoneR=1 :- ~p, DoneL=1.', [Inv], Res).
 
 mk_query_naming(Res) :-
 	findall(X, register(X), Vs),
 	mk_and(Vs, Vs1),
 	format_atom('query_naming(inv(~p, done, t)).', [Vs1], Res).
+
 mk_vcs(Res) :-
 	mk_init(Init),
-	mk_invariant(Inv),
-	mk_primed_invariant(Inv1),
-	mk_next_def(Next),
+	mk_relational_invariant(Inv),
+	mk_primed_relational_invariant(Inv1),
+	mk_next_def('L', NextL),
+	mk_next_def('R', NextR),
 	mk_property(Prop),
-	format_atom('~p:-~p~n~p :- ~p,~n~p.~n~p~n',[Inv,Init,Inv1,Next,Inv,Prop], Res).
+	format_atom('~p:-~p~n~p :- ~p,~n~p,~n~p.~n~p~n',[Inv,Init,Inv1,NextL,NextR,Inv,Prop], Res).
 
 cleanup :-
 	retractall(assigned).
@@ -142,7 +163,8 @@ mk_output_file(Res) :-
 	format_atom('~p~n~p~n~p~n',[Naming,Next,Vcs], Res).
 	
 test :-
-	consult(['../examples/minimal']),
+	consult(['../examples/minimal/stall-my.pl']),
 	cleanup,
+	%mk_output_file(Res),
 	mk_output_file(Res),
 	format('~p',[Res]).
